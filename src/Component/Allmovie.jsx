70c20893d161a2e4bd74f2e528a2ac96ff2051bd
Golam  Dastogir <rudro1998@gmail.com>
2024-12-08 16:01:06 +0600

    search

--- src/Component/Allmovie.jsx
+++ src/Component/Allmovie.jsx
@@ -5,12 +5,26 @@ import Gotallmovie from './Gotallmovie';
 const Allmovie = () => {
     const loaders = useLoaderData()
     console.log(loaders)
-    
-    
+    const [search,setSearch] =useState()
+    useEffect(()=> {
+      fetch(`http://localhost:8000/us?searchParams=${search}`)
+      .then((res) => res.json())
+      .then((data)=> {
+        console.log(data)
+      })
+    },[search])
    
     return (
      <div>
-        
+           <div className="my-6 flex items-center justify-center">
+                <input
+                    className="w-1/2 py-2 px-2 border-2 rounded-xl"
+                    type="text"
+                    onChange={(e) => setSearch(e.target.value)}
+                    name="search"
+                    placeholder="Search movies backend"
+                />
+            </div>
            <div className='md:grid justify-center items-center grid-cols-3 gap-5 shadow-lg border-2 rounded-lg bg-gray-300 my-4 m-1 py-8 px-4'>
             {
                 loaders.map((loader)=> (
